0158bb6a4a6ef156c27bc6e7b791fc912b92ace7
Olivier ORAND <olivier.orand@orange.com>
2018-05-04 14:18:34 +0000

    add orage custo

---
+++ cluster/deploy-concourse.sh
@@ -0,0 +1,17 @@
+set -x
+bosh -e ${BOSH_ENVIRONMENT} deploy -d concourse concourse.yml \
+   -o operations/basic-auth.yml \
+   -o operations/static-web.yml \
+   -o operations/tls.yml \
+   -o operations/tls-vars.yml \
+   -o operations/listen-on-vip.yml \
+   -l ../versions.yml \
+   -v network_name=concourse-bucc \
+   -v external_url=https://84.39.34.251 \
+   -v external_ip=84.39.34.251 \
+   -v web_ip=192.168.80.201 \
+   --var web_vm_type=large \
+   --var db_vm_type=large \
+   --var db_persistent_disk_type=db \
+   --var worker_vm_type=worker \
+   -v deployment_name=concourse
---
+++ cluster/generate-values-for-credhub.sh
@@ -0,0 +1,3 @@
+
+bucc credhub
+credhub generate -n="/bosh_bucc/concourse/atc_basic_auth" -t user
